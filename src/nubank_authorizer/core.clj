ada944cfc0e98dc7f2b6c1455a887d8dae4bf14c
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 17:40:35 -0300

    adapter: create edn to json conversion

--- src/nubank_authorizer/core.clj
+++ src/nubank_authorizer/core.clj
@@ -17,12 +17,18 @@
 (defn json-to-edn-adapter [json]
   (json/read-str json :key-fn keyword))
 
+(defn edn-to-json-adapter [edn]
+  (if (nil? edn)
+    ""
+    (str (json/write-str edn) "\n")))
+
 (defn cli-port []
   (doseq [input-line (line-seq (java.io.BufferedReader. *in*))]
     (-> input-line
         json-to-edn-adapter
         business-logic
-        println)))
+        edn-to-json-adapter
+        print)))
 
 (defn -main
   [& args]

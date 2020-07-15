50f145b7b356358f6f7645cde15b8c90366237f1
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 17:52:55 -0300

    ports: move to its own namespace

--- src/nubank_authorizer/core.clj
+++ src/nubank_authorizer/core.clj
@@ -1,16 +1,8 @@
 (ns nubank-authorizer.core
   (:gen-class)
-  (:require [nubank-authorizer.business_logic :refer [business-logic]]
+  (:require [nubank-authorizer.ports :as ports]
             [nubank-authorizer.adapters :as adapters]))
-
-(defn cli-port []
-  (doseq [input-line (line-seq (java.io.BufferedReader. *in*))]
-    (-> input-line
-        adapters/json-to-edn
-        business-logic
-        adapters/edn-to-json
-        print)))
 
 (defn -main
   [& args]
-  (cli-port))
+  (ports/cli))
---
+++ src/nubank_authorizer/ports.clj
@@ -0,0 +1,11 @@
+(ns nubank-authorizer.ports
+  (:require [nubank-authorizer.business_logic :refer [business-logic]]
+            [nubank-authorizer.adapters :as adapters]))
+
+(defn cli []
+  (doseq [input-line (line-seq (java.io.BufferedReader. *in*))]
+    (-> input-line
+        adapters/json-to-edn
+        business-logic
+        adapters/edn-to-json
+        print)))

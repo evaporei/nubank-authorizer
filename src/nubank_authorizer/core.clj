f031c92cf0e772aa7c50a62149d0a8d69895ccd7
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 17:23:58 -0300

    main: move reading stdin to cli-port

--- src/nubank_authorizer/core.clj
+++ src/nubank_authorizer/core.clj
@@ -1,7 +1,11 @@
 (ns nubank-authorizer.core
   (:gen-class))
 
+(defn cli-port []
+  (doseq [input-line (line-seq (java.io.BufferedReader. *in*))]
+    (-> input-line
+        println)))
+
 (defn -main
   [& args]
-  (doseq [input-line (line-seq (java.io.BufferedReader. *in*))]
-    (println input-line)))
+  (cli-port))

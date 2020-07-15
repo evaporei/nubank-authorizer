8c86c5eeef1694e9da222a4822ee8104c0a749f5
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 17:33:22 -0300

    business-logic: add layer without content

--- src/nubank_authorizer/core.clj
+++ src/nubank_authorizer/core.clj
@@ -2,6 +2,18 @@
   (:gen-class)
   (:require [clojure.data.json :as json]))
 
+(defn create-account [data]
+  data)
+
+(defn authorize-transaction [data]
+  data)
+
+(defn business-logic [data]
+  (cond
+    (contains? data :account) (create-account data)
+    (contains? data :transaction) (authorize-transaction data)
+    :else nil))
+
 (defn json-to-edn-adapter [json]
   (json/read-str json :key-fn keyword))
 
@@ -9,6 +21,7 @@
   (doseq [input-line (line-seq (java.io.BufferedReader. *in*))]
     (-> input-line
         json-to-edn-adapter
+        business-logic
         println)))
 
 (defn -main

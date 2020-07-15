7d4734aa623d161301cac51709a871d5b4f6a934
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 17:26:35 -0300

    adapter: create json to edn conversion

--- src/nubank_authorizer/core.clj
+++ src/nubank_authorizer/core.clj
@@ -1,9 +1,14 @@
 (ns nubank-authorizer.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json]))
+
+(defn json-to-edn-adapter [json]
+  (json/read-str json :key-fn keyword))
 
 (defn cli-port []
   (doseq [input-line (line-seq (java.io.BufferedReader. *in*))]
     (-> input-line
+        json-to-edn-adapter
         println)))
 
 (defn -main

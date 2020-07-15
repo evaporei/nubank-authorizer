e385aaea2084760998027deadfde69bbf0c4bda4
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 17:09:42 -0300

    project: add lein-binplus for binary generation

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :main ^:skip-aot nubank-authorizer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:plugins [[lein-binplus "0.6.6"]]}})

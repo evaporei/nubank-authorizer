055cce09feca4c52f3c519ab9d57b793a1457054
Otávio Pace <otaviopp8@gmail.com>
2020-07-16 14:07:40 -0300

    controller: make routing more pure and test authorize-transaction

--- src/nubank_authorizer/controller.clj
+++ src/nubank_authorizer/controller.clj
@@ -18,16 +18,22 @@
     (db/create-transaction! storage (:transaction output))
     output))
 
-(defn routing [storage input-data]
+(defn routing [input-data]
   (cond
     (contains? input-data :account)
-      (create-account! storage input-data)
+      [create-account! input-data]
     (contains? input-data :transaction)
-      (authorize-transaction! storage input-data)
-    :else nil))
+      [authorize-transaction! input-data]
+    :else
+      [(constantly nil) input-data]))
+
+(defn execute-controller! [storage controller-and-input]
+  (let [[controller input-data] controller-and-input]
+      (controller storage input-data)))
 
 (defn controller [storage input]
   (->> input
        adapters/json-to-edn
-       (routing storage)
+       routing
+       (execute-controller! storage)
        adapters/edn-to-json))
--- test/nubank_authorizer/controller_test.clj
+++ test/nubank_authorizer/controller_test.clj
@@ -3,12 +3,41 @@
             [nubank-authorizer.controller :refer :all]
             [nubank-authorizer.database :refer [new-database]]))
 
-(deftest integration-success
-  (testing "Should perform all operations and return JSON"
+(deftest integration-create-account
+  (testing "Should perform all account operations and return JSON"
     (let [account "{\"account\":{\"activeCard\":true,\"availableLimit\":100}}"
           expected "{\"account\":{\"activeCard\":true,\"availableLimit\":100},\"violations\":[]}\n"]
       (is (= (controller (new-database) account) expected)))))
 
+(deftest integration-authorize-transaction
+  (testing "Should perform all transaction operations and return JSON"
+    (let [db (new-database)
+          account "{\"account\":{\"activeCard\":true,\"availableLimit\":100}}"
+          expected-operation1 "{\"account\":{\"activeCard\":true,\"availableLimit\":100},\"violations\":[]}\n"
+          transaction "{\"transaction\":{\"merchant\":\"Burger King\",\"amount\":20,\"time\":\"2019-02-13T10:00:00.000Z\"}}"
+          expected-operation2 "{\"account\":{\"activeCard\":true,\"availableLimit\":80},\"violations\":[]}\n"]
+      (is (= (controller db account) expected-operation1))
+      (is (= (controller db transaction) expected-operation2)))))
+
 (deftest integration-else
   (testing "Should perform nothing and return an empty String"
     (is (= (controller (new-database) "{\"nothing\":\"related\"}") ""))))
+
+(deftest routing-create-account
+  (testing "Should return create-account! along with input-data"
+    (is (= (routing {:account {}}) [create-account! {:account {}}]))))
+
+(deftest routing-authorize-transaction
+  (testing "Should return authorize-transaction! along with input-data"
+    (is (= (routing {:transaction {}}) [authorize-transaction! {:transaction {}}]))))
+
+(deftest routing-else
+  (testing "Should return fn that returns nil along with input-data"
+    (let [[controller-fn data] (routing {:nothing :related})]
+      (is (= (controller-fn) nil))
+      (is (= data {:nothing :related})))))
+
+(deftest execute-controller-with-fn
+  (testing "Should return fn passed with storage and input-data"
+    (let [result (execute-controller! {:storage {}} [(constantly {:a :b})])]
+      (is (= result {:a :b})))))

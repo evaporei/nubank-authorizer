99100c7b6695e66406b8d9699dab3963cc520e6c
Otávio Pace <otaviopp8@gmail.com>
2020-07-15 22:56:49 -0300

    adapters: use camel-snake-kebab to convert JSON/EDN keys

--- src/nubank_authorizer/adapters.clj
+++ src/nubank_authorizer/adapters.clj
@@ -1,10 +1,11 @@
 (ns nubank-authorizer.adapters
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json]
+            [camel-snake-kebab.core :as csk]))
 
 (defn json-to-edn [json]
-  (json/read-str json :key-fn keyword))
+  (json/read-str json :key-fn csk/->kebab-case-keyword))
 
 (defn edn-to-json [edn]
   (if (nil? edn)
     ""
-    (str (json/write-str edn) "\n")))
+    (str (json/write-str edn :key-fn csk/->camelCaseString) "\n")))
--- test/nubank_authorizer/business_logic_test.clj
+++ test/nubank_authorizer/business_logic_test.clj
@@ -4,17 +4,17 @@
 
 (deftest create-account-empty-database
   (testing "Should return the input when database hasn't an account"
-    (let [account {:account {:activeCard true
-                             :availableLimit 100}}]
+    (let [account {:account {:active-card true
+                             :available-limit 100}}]
       (is (= (create-account nil account) account)))))
 
 (deftest create-account-
   (testing "Should return the input with `:violations` when database already has account"
-    (let [db-account {:activeCard true
-                      :availableLimit 100}
-          account {:account {:activeCard true
-                             :availableLimit 100}}
-          expected {:account {:activeCard true
-                              :availableLimit 100}
+    (let [db-account {:active-card true
+                      :available-limit 100}
+          account {:account {:active-card true
+                             :available-limit 100}}
+          expected {:account {:active-card true
+                              :available-limit 100}
                     :violations [:account-already-initialized]}]
       (is (= (create-account db-account account) expected)))))
